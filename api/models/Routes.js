4d689808e6327aad2e65ba1564c91446e234efee
Ivan Savelyev <err4o4@MacBook-Pro-Ivan.local>
2015-06-05 23:48:23 +0300

    Routes added

--- api/models/Routes.js
+++ api/models/Routes.js
@@ -9,23 +9,24 @@ module.exports = {
 
   attributes: {
 
-  	authorID		: { type: 'string' },
+  	authorID		: { type: 'string' }, // ID автора
 
-  	title			: { type: 'string' },
-  	description		: { type: 'JSON' },
+  	title			: { type: 'string' }, // Заголовок маршрута
+  	description		: { type: 'JSON' },	
+  	// Полное описание маршрута: [tags, text, startPointName, endPointName, distance, duration, climb, level]
 
-  	type			: { type: 'string' },
-  	waypoints		: { type: 'JSON' },
+  	type			: { type: 'string' }, // Тип маршрута (город, деревня и тд)
+  	waypoints		: { type: 'JSON' }, // Точки по которым строится маршрут на карте
 
   	comments		: { type: 'JSON' },
   	likes			: { type: 'string' },
-  	ridesCounter	: { type: 'string' },
+  	ridesCounter	: { type: 'string' }, // Счетчик людей, которые катались по данному маршруту 
   	views			: { type: 'string' },
-  	relatedRoutes	: { type: 'JSON' },
+  	relatedRoutes	: { type: 'JSON' }, // ID похожих маршрутов 
 
-  	status			: { type: 'string' },
-  	GUID			: { type: 'string' }
-  	
+  	status			: { type: 'string' }, // Статус маршрута (одобрен, забанен...)
+  	GUID			: { type: 'string' } // Абсолютная ссылка на маршрут
+
   }
 };
 
